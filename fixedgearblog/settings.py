5eaab48e4c0e725464d361b4ae76ef9660cbe95b
evanhkrupa@gmail.com <evanhkrupa@gmail.com>
2025-11-23 17:18:17 -0500

    settings host

--- fixedgearblog/settings.py
+++ fixedgearblog/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = 'SECRET_KEY'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['54.144.238.122', 'localhost', 'fixedgearsolace.com']
+ALLOWED_HOSTS = ['54.144.238.122', 'localhost', 'www.fixedgearsolace.com']
 
 
 # Application definition
