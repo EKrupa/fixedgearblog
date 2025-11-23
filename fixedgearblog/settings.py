c0f28a2327df62254b1599b330a2a6242a1a6beb
evanhkrupa@gmail.com <evanhkrupa@gmail.com>
2025-11-23 07:27:16 -0500

    settings update

--- fixedgearblog/settings.py
+++ fixedgearblog/settings.py
@@ -8,8 +8,6 @@ env=environ.Env()
 environ.Env.read_env()
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-SECRET_KEY = os.environ.get('SECRET_KEY')
-SECRET_KEY=env('SECRET_KEY')
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
