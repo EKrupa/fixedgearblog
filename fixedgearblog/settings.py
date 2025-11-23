46450eae3434eba426994b681dbb10410a332678
Evan Krupa <evanhkrupa@gmail.com>
2025-11-23 07:35:24 -0500

    Set DEBUG to False for production settings

--- fixedgearblog/settings.py
+++ fixedgearblog/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'SECRET_KEY'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['54.144.238.122', 'localhost', 'fixedgearsolace.com']
 
