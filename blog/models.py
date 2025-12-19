730b0dddaac81b8435d53e8399824297d7b66546
Evan Krupa <evanhkrupa@gmail.com>
2025-12-19 12:45:40 +0000

    Change image upload path in Gear model

--- blog/models.py
+++ blog/models.py
@@ -16,7 +16,7 @@ class Gear(models.Model):
     name = models.CharField(max_length=200)
     brand = models.CharField(max_length=100)
     description = models.TextField()
-    image = models.ImageField(upload_to='static/images', null=True, blank=True)
+    image = models.ImageField(upload_to='images/', null=True, blank=True)
     link = models.URLField(max_length=200, null=True, blank=True)
     
     def __str__(self):
